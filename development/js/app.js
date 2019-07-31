44069f8b3e1422a767050d8b26244c1cad22b792
Wojciech Pluta <wojciech.pluta1@gmail.com>
2019-07-31 13:09:33 +0200

    Logowanie

---
+++ development/js/app.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded',function () {
+    var btn = document.querySelector("button");
+    var articleform=document.querySelector('.form')
+    var aName=document.querySelector('a.aName');
+    var errorMessage=document.querySelector('.errorMessage');
+
+
+
+    if(localStorage.getItem("name")!==null) {
+        articleform.classList.add("hide");
+        var icon='<i class="far fa-user-circle"></i>'; //ikona przy imieniu
+        aName.innerHTML=localStorage.getItem("name")+icon;
+    }
+    btn.addEventListener('click', function (event) {
+        if(document.getElementById("name").value.length<=0)
+        {
+            errorMessage.innerText="Nazwa użytkownika musi zawierać conajmniej jeden znak";
+            event.preventDefault();
+        }
+        else {
+            var userName = document.getElementById("name").value;
+            localStorage.setItem("name", userName);
+            articleform.classList.add("hide");
+            location.reload();
+        }
+    });
+
+});
