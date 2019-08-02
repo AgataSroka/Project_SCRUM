84a086fa960ff9b355673fd70e4451151a45f998
Agata Połetek <poletekagata@gmail.com>
2019-08-02 19:34:02 +0200

    wyswietlanie przepisow-aktualizacja3

--- development/js/ap_recipes.js
+++ development/js/ap_recipes.js
@@ -1,4 +1,30 @@
-document.addEventListener("DOMContentLoade")
-function renderAllRecipes(){
+document.addEventListener('DOMContentLoaded',function () {
+    function renderAllRecipes() {
 
-}
+        allRecipesContainer.innerHTML = "";
+
+        var allRecipes = JSON.parse(localStorage.getItem("recipes"));
+
+        var newList = document.createElement("tr");
+
+        allRecipes.forEach(function(singleRecipe) {
+
+            var newLi = document.createElement("td");
+
+            newLi.innerHTML = singleRecipe.title;
+
+            var ingredientsUl = document.createElement("tr");
+            newLi.appendChild(ingredientsUl);
+
+            singleRecipe.ingredients.forEach(function(ingredient) {
+
+                var newRecipeLi = document.createElement("td");
+                newRecipeLi.innerHTML = ingredient;
+                ingredientsUl.appendChild(newRecipeLi);
+            });
+
+            newList.appendChild(newLi);
+            allRecipesContainer.appendChild(newList);
+        });
+    }
+});
